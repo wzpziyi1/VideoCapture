d02ee9392388ec89989cd79242ecd06ae2219b39
wzpziyi1 <1204473090@qq.com>
2017-06-14 08:36:21 +0800

    初始化视频的输入输出

--- VideoCapture/ViewController.m
+++ VideoCapture/ViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
 
+@property (nonatomic, strong) AVCaptureSession *session;
+
+@property (nonatomic, strong) AVCaptureDeviceInput *videoInput;
+
+@property (nonatomic, strong) AVCaptureVideoDataOutput *videoOutput;
 @end
 
 @implementation ViewController
@@ -17,13 +23,75 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //初始化视频的输入&输出
+    [self setupVideoInputOutput];
+    
+    //初始化音频的输入&输出
+    [self setupAudioInputOutput];
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupVideoInputOutput
+{
+    //添加视频的输入
+    NSArray *devices = [AVCaptureDevice devices];
+    AVCaptureDevice *inputDevice;
+    for (AVCaptureDevice *tmpDevice in devices)
+    {
+        if (tmpDevice.position == AVCaptureDevicePositionFront)
+        {
+            inputDevice = tmpDevice;
+            break;
+        }
+    }
+    NSError *error = nil;
+    self.videoInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:&error];
+    if (error)
+    {
+        NSLog(@"%@", error);
+        return;
+    }
+    
+    //添加视频的输出
+    self.videoOutput = [[AVCaptureVideoDataOutput alloc] init];
+    //在输出SampleBuffer的时候不要卡住主线程
+    [self.videoOutput setSampleBufferDelegate:self queue:dispatch_get_global_queue(0, 0)];
+    
+    //添加输入&&输出
+    [self addInputOutputToSession:self.videoInput output:self.videoOutput];
 }
 
+- (void)setupAudioInputOutput
+{
+    
+}
+
+- (void)addInputOutputToSession:(AVCaptureDeviceInput *)input output:(AVCaptureVideoDataOutput *)output
+{
+    [self.session beginConfiguration];
+    
+    if ([self.session canAddInput:input])
+    {
+        [self.session addInput:input];
+    }
+    
+    if ([self.session canAddOutput:output])
+    {
+        [self.session addOutput:output];
+    }
+    
+    [self.session commitConfiguration];
+}
+#pragma mark - getter && setter
+
+- (AVCaptureSession *)session
+{
+    if (!_session)
+    {
+        _session = [[AVCaptureSession alloc] init];
+    }
+    return  _session;
+}
 
 @end
